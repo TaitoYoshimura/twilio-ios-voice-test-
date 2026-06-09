e6e5e14323e3a083d1aaf03b3e4ac0498f246810
TaitoYoshimura <pz6f9vej@s.okayama-u.ac.jp>
2026-06-09 15:21:51 +0900

    first commit

---
+++ twilio-ios-voice-test/twilio_ios_voice_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  twilio_ios_voice_testApp.swift
+//  twilio-ios-voice-test
+//
+//  Created by yoshimura on 2026/06/09.
+//
+
+import SwiftUI
+
+@main
+struct twilio_ios_voice_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
